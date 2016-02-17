930aba69e1a22b1028d32603a3ef271aa02124b1
Ali-Shaikh <aliakbarshaikh@live.co.uk>
2016-02-17 21:43:23 +0000

    Changed Login Tab Password Text Box PasswordChar property to “*”. Added controls to Register Tab.

--- CSharp_LoginSystemSQLCE/Form1.Designer.cs
+++ CSharp_LoginSystemSQLCE/Form1.Designer.cs
@@ -38,8 +38,18 @@
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtRegisterUsername = new System.Windows.Forms.TextBox();
+            this.txtRegisterPassword = new System.Windows.Forms.TextBox();
+            this.txtRegisterConfirmPassword = new System.Windows.Forms.TextBox();
+            this.txtRegisterEmail = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -90,7 +100,7 @@
             // 
             // txtLoginUsername
             // 
-            this.txtLoginUsername.Location = new System.Drawing.Point(105, 64);
+            this.txtLoginUsername.Location = new System.Drawing.Point(105, 65);
             this.txtLoginUsername.Name = "txtLoginUsername";
             this.txtLoginUsername.Size = new System.Drawing.Size(130, 20);
             this.txtLoginUsername.TabIndex = 3;
@@ -113,6 +123,15 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.btnRegister);
+            this.tabPage2.Controls.Add(this.txtRegisterEmail);
+            this.tabPage2.Controls.Add(this.txtRegisterConfirmPassword);
+            this.tabPage2.Controls.Add(this.txtRegisterPassword);
+            this.tabPage2.Controls.Add(this.txtRegisterUsername);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -139,6 +158,81 @@
             this.tabPage4.Text = "Configuraton";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Username: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Password: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Confirm: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Email: ";
+            // 
+            // txtRegisterUsername
+            // 
+            this.txtRegisterUsername.Location = new System.Drawing.Point(105, 54);
+            this.txtRegisterUsername.Name = "txtRegisterUsername";
+            this.txtRegisterUsername.Size = new System.Drawing.Size(130, 20);
+            this.txtRegisterUsername.TabIndex = 4;
+            // 
+            // txtRegisterPassword
+            // 
+            this.txtRegisterPassword.Location = new System.Drawing.Point(105, 90);
+            this.txtRegisterPassword.Name = "txtRegisterPassword";
+            this.txtRegisterPassword.PasswordChar = '*';
+            this.txtRegisterPassword.Size = new System.Drawing.Size(130, 20);
+            this.txtRegisterPassword.TabIndex = 5;
+            // 
+            // txtRegisterConfirmPassword
+            // 
+            this.txtRegisterConfirmPassword.Location = new System.Drawing.Point(105, 126);
+            this.txtRegisterConfirmPassword.Name = "txtRegisterConfirmPassword";
+            this.txtRegisterConfirmPassword.PasswordChar = '*';
+            this.txtRegisterConfirmPassword.Size = new System.Drawing.Size(130, 20);
+            this.txtRegisterConfirmPassword.TabIndex = 6;
+            // 
+            // txtRegisterEmail
+            // 
+            this.txtRegisterEmail.Location = new System.Drawing.Point(105, 162);
+            this.txtRegisterEmail.Name = "txtRegisterEmail";
+            this.txtRegisterEmail.Size = new System.Drawing.Size(130, 20);
+            this.txtRegisterEmail.TabIndex = 7;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(105, 204);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.TabIndex = 8;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -150,6 +244,8 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -166,6 +262,15 @@
         private System.Windows.Forms.TextBox txtLoginUsername;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.TextBox txtRegisterEmail;
+        private System.Windows.Forms.TextBox txtRegisterConfirmPassword;
+        private System.Windows.Forms.TextBox txtRegisterPassword;
+        private System.Windows.Forms.TextBox txtRegisterUsername;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
 
